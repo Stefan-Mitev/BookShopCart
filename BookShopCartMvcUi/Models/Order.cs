fb8e65683f8fb9283ff9d4e4bbad0ee1d835e6c2
Stefan-Mitev <123822245+Stefan-Mitev@users.noreply.github.com>
2024-12-28 20:26:50 +0200

    1 part done

---
+++ BookShopCartMvcUi/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Framework;
+using NuGet.Protocol;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+namespace BookShopCartMvcUi.Models
+{
+    [Table("Order")]
+    public class Order
+    {
+        public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.UtcNow;
+        [Required]
+        public int OrderStatusId { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public OrderStatus OrderStatus { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+    }
+}
